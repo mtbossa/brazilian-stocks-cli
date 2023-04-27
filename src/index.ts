422be8057f0ff837ef411d9e38e58336ed3671a2
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2023-04-27 14:50:57 -0300

    Add spinner and chalk

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 /* eslint @typescript-eslint/no-floating-promises: 0 */
 import prompts from "prompts";
+import { Spinner } from "cli-spinner";
+import chalk from "chalk";
+
 import { print_new_line } from "./helpers/new_line";
 import { AvailableStrategies } from "strategies/available-strategies";
 import statusInvest from "scrapers/status-invest/status-invest";
@@ -25,9 +28,15 @@ import magicFormula from "strategies/magic-formula/magic-formula";
     ]);
 
     if (response.strategy === AvailableStrategies.MagicFormula) {
+        const spinner = new Spinner(
+            `${chalk.blue.bold("%s")} Calculando ${chalk.green.bold("Magic Formula ranking")}...`
+        );
+        spinner.setSpinnerString(19);
+        spinner.start();
         const result = await statusInvest.scrape();
         const parsed = result.map((stock) => statusInvest.parseToStock(stock));
         const ranked = magicFormula.calculate(parsed);
+        spinner.stop(true);
         console.table(ranked);
     }
 })();
