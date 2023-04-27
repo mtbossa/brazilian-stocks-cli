048da3c572d8c6b1922d13531be89138b52565cf
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2023-04-27 11:54:06 -0300

    Configure tsconfig to accept path mappings

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
+/* eslint @typescript-eslint/no-floating-promises: 0 */
 import prompts from "prompts";
 import { print_new_line } from "./helpers/new_line.js";
 
-print_new_line();
-console.log("Estratégia de ações brasileiras.");
-print_new_line();
+(async () => {
+    print_new_line();
+    console.log("Estratégia de ações brasileiras.");
+    print_new_line();
 
-const response = await prompts([
-    {
-        type: "select",
-        name: "strategy",
-        message: `Selecione uma estratégia para obter os resultados atuais:`,
-        choices: [{ title: "Magic Formula - Joel Greenblatt", value: "MAGIC_FORMULA" }],
-    },
-]);
+    const response = await prompts([
+        {
+            type: "select",
+            name: "strategy",
+            message: `Selecione uma estratégia para obter os resultados atuais:`,
+            choices: [{ title: "Magic Formula - Joel Greenblatt", value: "MAGIC_FORMULA" }],
+        },
+    ]);
+})();
