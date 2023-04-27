6b348247820f7882b3f4a37cb5afa08bc177d483
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2023-04-27 08:31:19 -0300

    Testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello, world!");
+import prompts from "prompts";
+
+const response = await prompts([
+    {
+        type: "text",
+        name: "twitter",
+        message: `What's your twitter handle?`,
+    },
+    {
+        type: "multiselect",
+        name: "color",
+        message: "Pick colors",
+        choices: [
+            { title: "Red", value: "#ff0000" },
+            { title: "Green", value: "#00ff00" },
+            { title: "Blue", value: "#0000ff" },
+        ],
+    },
+]);
+
+console.log({ response });
+
+// => { twitter: 'terkelg', color: [ '#ff0000', '#0000ff' ] }
